0990037ca6920e52e45eb2708d9deccc4bf82b91
Irshad <irshad@millicent.in>
2026-01-31 15:30:37 +0530

    refactor: update invoice model to use numeric invoiceNo and implement counter for unique invoice numbers

--- src/controllers/invoice.controller.js
+++ src/controllers/invoice.controller.js
@@ -5,8 +5,7 @@ import { Invoice } from "../models/invoice.models.js";
 
 const createInvoice = asyncHandler(async (req, res) => {
     const {
-        invoiceNo,
-        invoiceDate,
+        // invoiceDate,
         clientName,
         address,
         items,
@@ -18,23 +17,16 @@ const createInvoice = asyncHandler(async (req, res) => {
         quotationId
     } = req.body;
 
-    if (!invoiceNo || !clientName || !address || !quotationId) {
-        throw new ApiError(400, "Required file is missing!");
+    if (!clientName || !address || !quotationId) {
+        throw new ApiError(400, "Required field is missing!");
     }
 
     if (!Array.isArray(items) || items.length === 0) {
         throw new ApiError(400, "At least one invoice item is required.");
     }
 
-    const existingInvoice = await Invoice.findOne({ invoiceNo });
-
-    if (existingInvoice) {
-        throw new ApiError(400, "Invoice number already exists.");
-    }
-
     const newInvoice = await Invoice.create({
-        invoiceNo,
-        invoiceDate: invoiceDate ? new Date(invoiceDate) : new Date(),
+        // invoiceDate: invoiceDate ? new Date(invoiceDate) : new Date(),
         clientName,
         address,
         items,
@@ -43,15 +35,15 @@ const createInvoice = asyncHandler(async (req, res) => {
         taxAmount: taxAmount || 0,
         discount: discount || 0,
         grandTotal,
-        quotationId: req.body.quotationId,
+        quotationId,
         createdBy: req.user._id,
         paymentStatus: "unpaid"
-    })
+    });
 
-    return res
-        .status(201)
-        .json(new ApiResponse(201, newInvoice, "Invoice created successfully!"));
-})
+    return res.status(201).json(
+        new ApiResponse(201, newInvoice, "Invoice created successfully!")
+    );
+});
 
 const getAllInvoices = asyncHandler(async (req, res) => {
     const filter = {}
@@ -67,19 +59,19 @@ const getAllInvoices = asyncHandler(async (req, res) => {
 })
 
 const getInvoiceById = asyncHandler(async (req, res) => {
-  const { invoiceId } = req.params;
+    const { invoiceId } = req.params;
 
-  const invoice = await Invoice.findById(invoiceId)
-    .populate("createdBy", "name email")
-    .lean();
+    const invoice = await Invoice.findById(invoiceId)
+        .populate("createdBy", "name email")
+        .lean();
 
-  if (!invoice) {
-    throw new ApiError(404, "Invoice not found");
-  }
+    if (!invoice) {
+        throw new ApiError(404, "Invoice not found");
+    }
 
-  return res.status(200).json(
-    new ApiResponse(200, invoice, "Invoice fetched successfully!")
-  );
+    return res.status(200).json(
+        new ApiResponse(200, invoice, "Invoice fetched successfully!")
+    );
 });
 
 
